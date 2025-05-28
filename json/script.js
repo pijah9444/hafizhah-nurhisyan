d32884027ace9fcbbdd502f02a064e46e40f3279
pijah9444 <pijahhia22hf@gmail.com>
2025-05-28 21:12:22 +0700

    melakukan adjust untuk mengambil data json

--- json/script.js
+++ json/script.js
@@ -6,12 +6,17 @@
 
 // console.log(mahasiswa);
 
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if(xhr.readystate == 4 && xhr.status == 200){
-        let mahasiswa =JSON.parse (this.responseText);
-        console.log(mahasiswa);
-    }
-}
-xhr.open('Get', 'coba.json', true);
-xhr.send();
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//     if(xhr.readystate == 4 && xhr.status == 200){
+//         let mahasiswa =JSON.parse (this.responseText);
+//         console.log(mahasiswa);
+//     }
+// }
+// xhr.open('Get', 'coba.json', true);
+// xhr.send();
+
+$.getJSON('coba.json', function (data)  {
+    console.log(data);
+
+});
