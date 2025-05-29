bc519a8f4bc5b09e82d7272498b1210e50c7ad90
pijah9444 <pijahhia22hf@gmail.com>
2025-05-29 07:46:47 +0700

    refactor: optimize menu display logic and improve code readability

--- wpu-hut/js/script.js
+++ wpu-hut/js/script.js
@@ -1,38 +1,38 @@
-function tampilkanSemuaMenu() {
-     $.getJSON('data/pizza.json', function (data) {
+function tampilkanSemuaMenu(){
+    $.getJSON('data/pizza.json', function(data){
         let menu = data.menu;
-       $.each (menu, function(i, data){
-        $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/'+ data.gambar+'"class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5 class="card-title">Rp. '+ data.harga +'</h5><a href="#" class="btn btn-primary">Order Now</a></div></div></div>')
-       });
+        let content = '';
+        // melakukan pengulangan
+        $.each(menu,function (i,data){
+            content +='<div class="col-md-4"><div class="card mb-3"><img src="img/menu/'+ data.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+data.nama+'</h5><p class="card-text">'+data.deskripsi+'</p><h5 class="card-title">Rp.'+data.harga+'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a> </div></div></div>'
+        });
+        $('#daftar-menu').html(content);
     });
 }
-
+// menampilkan fungsi class dimana waktu diklik berwarna ga hitam aja dan judulnya juga berusabah menjadi kategori yang dipanggil
 tampilkanSemuaMenu();
 
-
-$('.nav-link').on('click', function (){
+$('.nav-link').on('click', function(){
     $('.nav-link').removeClass('active');
     $(this).addClass('active');
 
     let kategori = $(this).html();
     $('h1').html(kategori);
 
-   if(kategori == 'All Menu') {
+    if(kategori == 'All Menu'){
         tampilkanSemuaMenu();
         return;
-   }
+    }
 
-
-   $.getJSON('data/pizza.json', function (data) {
-    let menu = data.menu;
-    
-
-    $.each(menu, function (i, data) {
-        if (data.kategori == kategori.toLowerCase()) {
-            content += '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/'+ data.gambar+'"class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5 class="card-title">Rp. '+ data.harga +'</h5><a href="#" class="btn btn-primary">Order Now</a></div></div></div>';
+    $.getJSON('data/pizza.json', function(data){
+       let menu = data.menu;
+       let content = '';
+       
+       $.each(menu, function (i,data){
+        if(data.kategori == kategori.toLowerCase()){
+            content += '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/'+ data.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+data.nama+'</h5><p class="card-text">'+data.deskripsi+'</p><h5 class="card-title">Rp.'+data.harga+'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a> </div></div></div>';
         }
+       });
+       $('#daftar-menu').html(content);
     });
-   });
-
-   $('#daftar-menu').html(content);
 });
